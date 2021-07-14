d88d920d1e86e9ddefcc78440c75690c4d6082bc
BrandonPerry-co <brandonperrymba@gmail.com>
2021-07-14 17:47:50 -0600

    Completed project

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,10 +1,21 @@
 import { combineReducers } from 'redux'
 
-const user = (state = null) => state
+const user = (state = null, action) => state
 
 // add switch statements in here
 const cars = (state = [], action) => {
-    return state
+    switch(action.type){
+        case "ADD_CAR" :
+            let newCar = [...state, action.value]
+            return newCar
+
+        case "REMOVE_CAR" :
+            let removeNewCar = [...state]
+            removeNewCar.splice(action.value, 1)
+            return removeNewCar
+
+        default : return state    
+    }
 }
 
 export default combineReducers({ user, cars })
